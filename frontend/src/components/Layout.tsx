80a8c911c1262020fd5b738a1d768055c100ec97
ps <piusharma68@gmail.com>
2024-10-02 21:11:57 +0530

    Add marker for current tab

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -11,35 +11,69 @@ import { useContext, type ReactNode } from "react";
 import { AuthContext } from "@/contexts/Auth";
 import { useNavigate } from "react-router-dom";
 
+const getActiveClass = (path: string, href: string) => {
+	const pathToken = path.split("/");
+	if (!pathToken[1] && href === "/") {
+		return "tw-bg-zinc-300";
+	}
+
+	if (pathToken[1] && pathToken[1] === href) {
+		return "tw-bg-zinc-300";
+	}
+	return "";
+};
+
 const Layout = () => {
+	const path = window.location.pathname;
+
 	return (
 		<NavigationMenu className="tw-h-full tw-justify-between tw-flex tw-flex-col tw-border-r-2">
 			<NavigationMenuList className="tw-h-full">
 				<NavigationMenuItem>
 					<NavigationMenuLink asChild>
 						<Link href="/">
-							<Button variant="link">Home</Button>
+							<Button
+								variant="link"
+								className={`tw-rounded-none ${getActiveClass(path, "/")}`}
+							>
+								Home
+							</Button>
 						</Link>
 					</NavigationMenuLink>
 				</NavigationMenuItem>
 				<NavigationMenuItem>
 					<NavigationMenuLink asChild>
 						<Link href="/dashboard">
-							<Button variant="link">Dashboard</Button>
+							<Button
+								variant="link"
+								className={`tw-rounded-none ${getActiveClass(path, "dashboard")}`}
+							>
+								Dashboard
+							</Button>
 						</Link>
 					</NavigationMenuLink>
 				</NavigationMenuItem>
 				<NavigationMenuItem>
 					<NavigationMenuLink asChild>
 						<Link href="/bookings">
-							<Button variant="link">Bookings</Button>
+							<Button
+								variant="link"
+								className={`tw-rounded-none ${getActiveClass(path, "bookings")}`}
+							>
+								Bookings
+							</Button>
 						</Link>
 					</NavigationMenuLink>
 				</NavigationMenuItem>
 				<NavigationMenuItem>
 					<NavigationMenuLink asChild>
 						<Link href="/analytics">
-							<Button variant="link">Analytics</Button>
+							<Button
+								variant="link"
+								className={`tw-rounded-none ${getActiveClass(path, "analytics")}`}
+							>
+								Analytics
+							</Button>
 						</Link>
 					</NavigationMenuLink>
 				</NavigationMenuItem>
